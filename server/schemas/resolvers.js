a9a01658a29ce8726441af076b0375d1b8366fb7
Jason (Jung Hyun) Yoo <jasonyoo3026@gmail.com>
2023-04-26 00:17:59 +1000

    added schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,66 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
+      throw new Error('You are not logged in!');
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ $or: [{ email }, { username: email }] });
+      if (!user) {
+        throw new Error("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new Error('Wrong password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, { bookInput }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookInput } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new Error('You must be logged in to save a book!');
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        if (!updatedUser) {
+          throw new Error("Couldn't find user with this id!");
+        }
+        return updatedUser;
+      }
+      throw new Error('You must be logged in to remove a book!');
+    },
+  },
+};
+
+module.exports = resolvers;
