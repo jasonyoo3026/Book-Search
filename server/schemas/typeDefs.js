a845878732e9680441bca23f6ed4041ca0452448
Jason (Jung Hyun) Yoo <jasonyoo3026@gmail.com>
2023-04-26 21:56:14 +1000

    modified typeDefs and created mutations.js and queries.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
-    _id: ID!
-    name: String!
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
   }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    me: User
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String!], description: String!, title: String!, bookId: String!, image: String, link: String): User
+    removeBook(bookId: String!): User
   }
 `;
 
